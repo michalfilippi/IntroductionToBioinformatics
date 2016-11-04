1c0dfd818d93a955ddac07d4b80fcb05c76ec76f
Michal Filippi <michal.filippi@gmail.com>
2016-11-04 17:17:09 +0100

    Minor bugs fixed.

--- constant_tables.py
+++ constant_tables.py
@@ -1,34 +1,34 @@
 dna_complement_mapping = {
-    'A' : 'T',
-    'T' : 'A',
-    'C' : 'G',
-    'G' : 'C'
+    'A': 'T',
+    'T': 'A',
+    'C': 'G',
+    'G': 'C'
     }
 dna_complement_mapping_table = str.maketrans(dna_complement_mapping)
 
 rna_complement_mapping = {
-    'A' : 'U',
-    'U' : 'A',
-    'C' : 'G',
-    'G' : 'C'
+    'A': 'U',
+    'U': 'A',
+    'C': 'G',
+    'G': 'C'
     }
 rna_complement_mapping_table = str.maketrans(rna_complement_mapping)
 
 rna2ps_mapping = {
     'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V',
-    'UUU': 'F', 'CUU': 'L', 'AUU': 'I', 'GUU': 'V',
+    'UUC': 'F', 'CUC': 'L', 'AUC': 'I', 'GUC': 'V',
     'UUA': 'L', 'CUA': 'L', 'AUA': 'I', 'GUA': 'V',
     'UUG': 'L', 'CUG': 'L', 'AUG': 'M', 'GUG': 'V',
     'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A',
-    'UCU': 'S', 'CCU': 'P', 'ACU': 'T', 'GCU': 'A',
+    'UCC': 'S', 'CCC': 'P', 'ACC': 'T', 'GCC': 'A',
     'UCA': 'S', 'CCA': 'P', 'ACA': 'T', 'GCA': 'A',
     'UCG': 'S', 'CCG': 'P', 'ACG': 'T', 'GCG': 'A',
     'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D',
-    'UAU': 'Y', 'CAU': 'H', 'AAU': 'N', 'GAU': 'D',
+    'UAC': 'Y', 'CAC': 'H', 'AAC': 'N', 'GAC': 'D',
     'UAA': 'X', 'CAA': 'Q', 'AAA': 'K', 'GAA': 'E',
     'UAG': 'X', 'CAG': 'Q', 'AAG': 'K', 'GAG': 'E',
-    'UGU': 'U', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G',
-    'UGU': 'U', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G',
+    'UGU': 'C', 'CGU': 'R', 'AGU': 'S', 'GGU': 'G',
+    'UGC': 'C', 'CGC': 'R', 'AGC': 'S', 'GGC': 'G',
     'UGA': 'X', 'CGA': 'R', 'AGA': 'R', 'GGA': 'G',
     'UGG': 'W', 'CGG': 'R', 'AGG': 'R', 'GGG': 'G'
     }

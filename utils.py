6332e3121ecde67247e0bea29837ae45268bf3aa
Michal Filippi <michal.filippi@gmail.com>
2016-11-04 12:41:20 +0100

    Function generating N-dimensional cube position predecessors created

--- utils.py
+++ utils.py
@@ -1,15 +1,35 @@
-def n_dimensional_iterator(*lengths):
-    """Generator of all positions in len(lengths)-dimensional cube,
-    where n-dimension has length lengths[n].
+def n_dim_cube_iterator(*lengths):
+    """Generator of all positions in len('lengths')-dimensional cube,
+    where n-dimension has length 'lengths'[n].
 
     :param lengths: lengths of dimensions
     :return: generator
     """
+
     if len(lengths) == 0:
         yield []
         return
+
     for i in range(lengths[0]):
-        for pos in n_dimensional_iterator(*lengths[1:]):
+        for pos in n_dim_cube_iterator(*lengths[1:]):
             yield [i] + pos
 
+
+def n_dim_cube_predecessors(position):
+    """Generates all preceding positions to 'position'. Preceding position is
+    position with all coordinates equal or lesser by one then original position.
+    First returned position is identical position.
+
+    :param position: position in n-dim cube
+    :return: generator of all preceding positions
+    """
+
+    if len(position) == 0:
+        yield []
+        return
+
+    for pos in n_dim_cube_predecessors(position[1:]):
+        yield [position[0]] + pos
+        if position[0] - 1 >= 0:
+            yield [position[0] - 1] + pos
 
